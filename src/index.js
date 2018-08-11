4de1caa2ebd1d189156e24bdd106d83b9511db4f
Attila Bagossy <bagossyattila@outlook.com>
2018-08-11 11:04:00 +0200

    Argument checks and slightly tweaked option-handling

--- src/index.js
+++ src/index.js
@@ -6,17 +6,7 @@ const parse = require('./downson/Parser');
 const Converter = require('./downson/Converter');
 const Failure = require('./downson/Failure');
 
-function checkArguments(src) {
-    if (typeof src === 'undefined' || src === null) {
-        throw new Error('downson(): input parameter is undefined or null');
-    }
-    if (typeof src !== 'string') {
-        throw new Error('downson(): input parameter is of type '
-            + Object.prototype.toString.call(src) + ', string expected');
-    }
-}
-
-function processMarkdown(src, options) {
+function marked(src, options) {
     const blockLexer = Object.create(BlockLexer);
     blockLexer.BlockLexer(options);
     const blockTokens = blockLexer.lex(src);
@@ -43,19 +33,25 @@ function processAsDownson(markdown) {
     };
 }
 
-function downson(src, options) {
-    checkArguments(src);
+function downson(input, options = { marked: {} }) {
+    if (typeof input === 'undefined' || input === null) {
+        throw new Error('downson(): input parameter is undefined or null');
+    }
+    if (typeof input !== 'string') {
+        throw new Error('downson(): input parameter is of type '
+            + Object.prototype.toString.call(input) + ', string expected');
+    }
 
     try {
-        const opts = Object.assign({}, downson.defaults, options || {});
+        const markedOptions = Object.assign({}, downson.defaultOptions.marked || {}, options.marked);
 
-        const markdown = processMarkdown(src, opts);        
+        const markdown = marked(input, markedOptions);
 
         return processAsDownson(markdown);
     } catch (e) {
         e.message += '\nPlease report this to https://github.com/battila7/downson-js.';
 
-        if ((options || downson.defaults).silent) {
+        if ((options || downson.defaultOptions).silent) {
             return {};
         }
 
@@ -63,40 +59,59 @@ function downson(src, options) {
     }
 }
 
-downson.options =
-    downson.setOptions = function (opt) {
-        Object.assign(downson.defaults, opt);
-        return downson;
-    };
-
-downson.getDefaults = function () {
+downson.getFactoryDefaults = function getFactoryDefaults() {
     return {
-        baseUrl: null,
-        breaks: false,
-        gfm: true,
-        headerIds: true,
-        headerPrefix: '',
-        highlight: null,
-        langPrefix: 'language-',
-        mangle: true,
-        pedantic: false,
-        sanitize: false,
-        sanitizer: null,
-        silent: false,
-        smartLists: false,
-        smartypants: false,
-        tables: true,
-        xhtml: false
+        marked: {
+            baseUrl: null,
+            breaks: false,
+            gfm: true,
+            headerIds: true,
+            headerPrefix: '',
+            highlight: null,
+            langPrefix: 'language-',
+            mangle: true,
+            pedantic: false,
+            sanitize: false,
+            sanitizer: null,
+            smartLists: false,
+            smartypants: false,
+            tables: true,
+            xhtml: false
+        },
+        silent: false
     };
 }
 
-downson.defaults = downson.getDefaults();
+downson.defaultOptions = downson.getFactoryDefaults();
 
 downson.registerType = function registerType(type, converterMethod) {
-    Converter.register(tyoe, converterMethod);
+    if (typeof type === 'undefined' || type === null || converterMethod === 'undefined' || converterMethod === null) {
+        throw new Error('registerType(): type or converterMethod is undefined/null.');
+    }
+
+    if (typeof type !== 'string') {
+        throw new Error('registerType(): type parameter is of type '
+            + Object.prototype.toString.call(src) + ', string expected');
+    }
+
+    if (typeof converterMethod !== 'function') {
+        throw new Error('registerType(): converterMethod parameter is of type '
+            + Object.prototype.toString.call(src) + ', function expected');
+    }
+
+    Converter.register(type, converterMethod);
 };
 
 downson.deregisterType = function deregisterType(type) {
+    if (typeof type === 'undefined' || type === null) {
+        throw new Error('deregisterType(): type is undefined/null.');
+    }
+
+    if (typeof type !== 'string') {
+        throw new Error('deregisterType(): type parameter is of type '
+            + Object.prototype.toString.call(src) + ', string expected');
+    }
+
     return Converter.deregister(type);
 }
 
